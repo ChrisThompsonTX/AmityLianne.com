cb8d36b306aace5a307b281d61ed9f80bf71242f
Chris Thompson <chris.tx86@gmail.com>
2020-07-11 21:41:13 -0700

    animate pinned top

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ function openInfo(e) {
 }
 
 imageContainers.forEach(image => image.addEventListener('click', openInfo));
-window.addEventListener('scroll', (e)=> console.log(e))
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+function checkSlide(e) {
+    const slideUpAt = (window.scrollY + window.innerHeight) -
+        (amity.height / 2);
+    const imageBottom = amity.offsetTop + amity.height;
+    const isHalfShown = slideUpAt > amity.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+        amity.classList.add('active');
+    } else {
+        amity.classList.remove('active');
+    }
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
